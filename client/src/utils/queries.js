9ac5904b927340faee302f532836f3f6008485d0
KevinGodek <92218951+KevinGodek@users.noreply.github.com>
2022-03-19 22:33:56 -0400

    added mutations and queries js files to utils and refactored searchbooks.js

---
+++ client/src/utils/queries.js
@@ -0,0 +1,22 @@
+import gql from 'graphql-tag';
+
+const getUser = gql`
+    {
+        me {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
+`;
+
+export default getUser;
